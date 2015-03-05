3b9b9337364367ac48cdb16ac4d7df813432dc0c
ercchy <erika@serox.si>
2015-03-05 09:51:59 +0100

    Initial commit

---
+++ js/demo.js
@@ -0,0 +1,12 @@
+/* global window console */
+
+(function (window) {
+	"use strict";
+
+	function HangoutDemo() {
+		console.log("Starting...");
+	}
+
+	var hangoutDemo = new HangoutDemo();
+
+}(window));
